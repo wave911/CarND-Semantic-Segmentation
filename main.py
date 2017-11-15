bd35baa2708849e83337008dbb6a0e3fb0a1f668
Alexander Epifanov <alexander.epifanov@here.com>
2017-11-15 13:57:50 +0100

    fixed eror

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def run():
                  keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
-        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_in)
 
         # OPTIONAL: Apply the trained model to a video
 
